183617a3aba5566bb1c31da836e6b1dab3769354
ahmed mecherouk <ahmedmecherouk@gmail.com>
2024-11-19 11:49:56 +0100

    created seeds

--- app/models/art.rb
+++ app/models/art.rb
@@ -3,7 +3,7 @@ class Art < ApplicationRecord
   has_many :bookings
   validates :title, presence: true
   validates :category, presence: true
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :creation_date, presence: true
   validates :price, presence: true
 end
