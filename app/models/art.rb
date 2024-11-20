d69c08746093d87067e893be27f855a8774759ee
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2024-11-20 12:33:58 +0100

    add style to cards

--- app/models/art.rb
+++ app/models/art.rb
@@ -2,7 +2,7 @@ class Art < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
-  
+
   validates :title, presence: true
   validates :category, presence: true
   validates :name, presence: true
