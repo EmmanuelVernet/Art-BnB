02627a609ab330765054b0d1201bc4a727c35dd0
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2024-11-21 11:04:55 +0100

    dashboard accessible when user loged

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, except: :dashboard
   def home
   end
 
   def dashboard
-   @bookings = current_user.bookings
+    @bookings = current_user.bookings
   end
 end
